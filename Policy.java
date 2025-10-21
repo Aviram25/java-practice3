cf3db52b517c6839b62426ac15697b6645d4b797
Aviram Dhagat <dhagataviram@gmail.com>
2025-10-21 12:48:23 +0530

    Create Policy.java

---
+++ Policy.java
@@ -0,0 +1,23 @@
+package day2;
+
+public class policy{
+	
+	/*
+	 * public policy(int regNo, String make, String model, int price) { super(regNo,
+	 * make, model, price); }
+	 */
+	
+	/*
+	 * public double calculateInsurance() { return price * 0.10; }
+	 */
+	
+	
+	public void displayInsurance(vehicle v)
+	{
+		/* System.out.println("Vehicle Details:"); */
+		/* System.out.println("Vehicle Insurance Details:"); */
+		/* System.out.println(v.calculateInsurance()); */
+	
+	}
+	
+}
